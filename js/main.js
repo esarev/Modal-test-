0847645ccb639e464b19dd539872805406c8ce0e
Alex <esarev@inbox.ru>
2021-11-05 14:57:08 +0700

    login

--- js/main.js
+++ js/main.js
@@ -2,7 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const callBackBtn = document.getElementById('callback-button'),
         modal = document.getElementById('modal-window'),
-        closeBtn = document.querySelector('.modal__close-button');
+        buttonOut = document.querySelector('.button-out'),
+        userName = document.querySelector('.user-name'),
+        closeBtn = document.querySelector('.modal__close-button'),
+        logInForm = document.getElementById('logInForm'),
+        name = document.getElementById('name'),
+        surname = document.getElementById('surname'),
+        email = document.getElementById('email'),
+        password = document.getElementById('password'),
+        date = document.getElementById('date');
+
+  const login = (user) => {
+    callBackBtn.style.display = 'none';
+
+    buttonOut.style.display = 'block';
+    userName.style.display = 'block';
+  
+    userName.textContent = user.name;
+    modal.classList.remove('modal_active');
+  };
+
+  const logout = () => {
+    callBackBtn.style.display = 'block';
+
+    buttonOut.style.display = 'none';
+    userName.style.display = 'none';
+    userName.textContent = '';
+  };
   
   callBackBtn.onclick = function () {
     modal.classList.add('modal_active');
@@ -11,4 +37,24 @@ document.addEventListener('DOMContentLoaded', () => {
   closeBtn.onclick = function () {
     modal.classList.remove('modal_active');
   };
+
+  buttonOut.addEventListener('click', () => {
+    logout();
+  });
+
+  logInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = {
+      name: name.value,
+      surname: surname.value,
+      email: email.value,
+      password: password.value,
+      date: date.value
+    };
+
+    login(user);
+  });
+
+
 });
