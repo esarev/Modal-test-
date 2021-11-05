5c93a54f5fc7c5b58e0db7e4e86d99c8f63813d4
Alex <esarev@inbox.ru>
2021-11-05 17:24:43 +0700

    validate

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const callBackBtn = document.getElementById('callback-button'),
         modal = document.getElementById('modal-window'),
+        form = document.getElementById('logInForm'),
+        formBtn = form.querySelector('.form-btn'),
         buttonOut = document.querySelector('.button-out'),
         userName = document.querySelector('.user-name'),
-        closeBtn = document.querySelector('.modal__close-button'),
-        logInForm = document.getElementById('logInForm'),
+        closeBtn = form.querySelector('.modal__close-button'),
         name = document.getElementById('name'),
         surname = document.getElementById('surname'),
         email = document.getElementById('email'),
         password = document.getElementById('password'),
-        date = document.getElementById('date');
+        passwordRepeat = document.getElementById('password-repeat'),
+        date = document.getElementById('date'),
+        fields = form.querySelectorAll('.fields');
 
   const login = (user) => {
     callBackBtn.style.display = 'none';
@@ -42,18 +45,47 @@ document.addEventListener('DOMContentLoaded', () => {
     logout();
   });
 
-  logInForm.addEventListener('submit', (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    //Validation:
+
+    for (let i = 0; i < fields.length; i++) {
+      if (!fields[i].value) {
+        console.log('пусто', fields[i]);
+
+        const error = document.createElement('div');
+        error.className = 'error';
+        error.style.color = 'red';
+        error.innerHTML = 'Заполните поле';
+        form[i].parentElement.insertBefore(error, fields[i]);
+      }
+    }
 
     const user = {
       name: name.value,
       surname: surname.value,
       email: email.value,
       password: password.value,
+      passwordRepeat: passwordRepeat.value,
       date: date.value
     };
 
-    login(user);
+    const errors = form.querySelectorAll('.error');
+
+    for (let i = 0; i < errors.length; i++) {
+      errors[i].remove();
+    }
+
+    if (password.value !== passwordRepeat.value) {
+      const error = document.createElement('div');
+      error.className = 'error';
+      error.style.color = 'red';
+      error.innerHTML = 'Пароли не совпадают';
+      passwordRepeat.parentElement.insertBefore(error, passwordRepeat);
+    }
+
+    // login(user);
   });
 
 
