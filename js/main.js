3953e0b7f77923c5ea67ec9decf7fdd24a014736
Alex <esarev@inbox.ru>
2021-11-09 00:24:51 +0700

    date

--- js/main.js
+++ js/main.js
@@ -126,40 +126,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   
   // Date
-//   let now = new Date();
-//   console.log(now);
-//   let userDate = new Date(now);
-//   console.log(userDate);
-//   let adult = now - userDate;
-//   console.log(adult);
-
-//   function isOverEighteen(birthday) {
-//       var ageDifMs = Date.now() - birthday.getTime();
-//       var ageDate = new Date(ageDifMs);
-//       var age = Math.abs(ageDate.getUTCFullYear() - 1970);
-//       if(age > 18){
-//         return true;
-//       }else{
-//         return false;
-//       }
-//   }
-
-// console.log(isOverEighteen(now));
-// console.log(isOverEighteen(userDate));
-
 
   inputDate.addEventListener('change', function() {
     let input = this.value;
+    let dateNow = new Date();
     let dateEntered = new Date(input);
     console.log(input);
     console.log(dateEntered);
-    // let sum = new Date() - dateEntered;
-    // console.log(sum);
-    
-    let sumFormatted = Math.abs(dateEntered.getUTCFullYear() - 1970);
-    console.log(sumFormatted);
-
-    if(sumFormatted > 18) {
+    let date = dateNow.getFullYear() - dateEntered.getFullYear();
+    console.log(date);
+    if(date > 18) {
       return true;
       } else {
       return false;
@@ -167,7 +143,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
   });
 
-  console.log(form);
   form.addEventListener('submit', function (e) {
     e.preventDefault();
 
